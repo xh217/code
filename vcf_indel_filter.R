14676d3f49f9959b78822a4c7d947718f0d4cf3a
Xiaoju_Hu <111913190+xh217@users.noreply.github.com>
2023-02-24 15:38:47 -0500

    Update vcf_indel_filter.R

--- vcf_indel_filter.R
+++ vcf_indel_filter.R
@@ -28,6 +28,6 @@ all1<-subset(all,AF>=AF_threshold)
 all1$sample<-Sample
 all1<-all1 %>% separate(name,sep=":",c("chr","pos")) %>% separate(pos,sep="_",c("pos","alt")) 
 all1<-all1[,c("sample","chr","pos","alt","AF")]
-keepchr<-c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX")
+keepchr<-paste0("chr",c(seq(1:22),"X"))
 all1<-all1[which(all1$chr%in%keepchr),]
 write.table(all1,paste0(indel_bcftools_path,"/",Sample,".indel"),quote=F,row.names=F)
